209eb3640e6a90d4bc797a36ce8c1b416966a2bd
Yuri Konev <crazy_clown93@mail.ru>
2025-04-14 13:20:24 +0300

    Task1

---
+++ HW_6/Task_1/Task_1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main() {
+    int hours;
+    setlocale(LC_ALL, "Russian");
+
+    
+    std::cout << "Введите, который час: ";
+    std::cin >> hours;
+
+    
+    while (hours < 0 || hours > 23) {
+        std::cout << "Ошибка: введите число от 0 до 23." << std::endl;
+        std::cout << "Введите, который час: ";
+        std::cin >> hours;
+    }
+
+    
+    for (int i = 0; i < hours; ++i) {
+        std::cout << "Ку-ку!" << std::endl;
+    }
+
+    return 0;
+}
