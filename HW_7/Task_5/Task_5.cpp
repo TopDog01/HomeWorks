cb929b0e428009896ca5530fded6b7ddbcf1e942
Yuri Konev <crazy_clown93@mail.ru>
2025-04-18 20:16:47 +0300

    5

--- HW_7/Task_5/Task_5.cpp
+++ HW_7/Task_5/Task_5.cpp
@@ -1,22 +1,45 @@
-﻿#include <iostream>
+﻿//#include <iostream>
+//
+//int main() {
+//    for (int y = 0; y <= 20; y++) { // Изменяем порядок, чтобы y был первым
+//        for (int x = 0; x <= 50; x++) {
+//            if (x == 25 && y == 10) // Точка пересечения
+//                std::cout << "+";
+//            else if (y == 10) // Горизонтальная ось
+//                std::cout << "-";
+//            else if (x == 25) // Вертикальная ось
+//                std::cout << "|";
+//            else if (x == 50 && y == 10) // Стрелка вправо
+//                std::cout << ">";
+//            else if (x == 25 && y == 0) // Стрелка вверх
+//                std::cout << "^";
+//            else
+//                std::cout << " ";
+//        }
+//        std::cout << "\n";
+//    }
+//    return 0;
+//}
+
+
+#include <iostream>
 
 int main() {
-    for (int y = 0; y <= 20; y++) { // Изменяем порядок, чтобы y был первым
-        for (int x = 0; x <= 50; x++) {
-            if (x == 25 && y == 10) // Точка пересечения
+    for (int x = 0; x <= 20; x++) {
+        for (int y = 0; y <= 50; y++) {
+            if (x == 10 && y == 25)
                 std::cout << "+";
-            else if (y == 10) // Горизонтальная ось
-                std::cout << "-";
-            else if (x == 25) // Вертикальная ось
-                std::cout << "|";
-            else if (x == 50 && y == 10) // Стрелка вправо
+            else if (x == 10 && y == 50)
                 std::cout << ">";
-            else if (x == 25 && y == 0) // Стрелка вверх
+            else if (x == 10)
+                std::cout << "-";
+            else if (y == 25 && x == 0)
                 std::cout << "^";
+            else if (y == 25)
+                std::cout << "|";
             else
                 std::cout << " ";
         }
         std::cout << "\n";
     }
-    return 0;
 }
