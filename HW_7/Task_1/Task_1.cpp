432ca65bd181c95dbc91efb50e4e7dc66396130b
Yuri Konev <crazy_clown93@mail.ru>
2025-04-18 08:34:51 +0300

    1

---
+++ HW_7/Task_1/Task_1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    const int initialBuckwheat = 100; 
+    const int consumptionPerMonth = 4; 
+
+    std::cout << "Всего гречки было на начало подсчёта: " << initialBuckwheat << " кг" << std::endl;
+
+    
+    for (int month = 1; initialBuckwheat - month * consumptionPerMonth >= 0; month++) {
+        int remainingBuckwheat = initialBuckwheat - month * consumptionPerMonth;
+        std::cout << "После " << month << " месяца у вас в запасе останется " << remainingBuckwheat << " кг гречки" << std::endl;
+    }
+
+    std::cout << "После " << (initialBuckwheat / consumptionPerMonth) << " месяца гречка закончится" << std::endl;
+
+    return 0;
+}
