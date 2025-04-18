2f59efaeb007120aeab5329ac009c11fd1a60c63
Yuri Konev <crazy_clown93@mail.ru>
2025-04-18 12:20:59 +0300

    5

---
+++ HW_7/Task_5/Task_5.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main() {
+    for (int y = 0; y <= 20; y++) { // Изменяем порядок, чтобы y был первым
+        for (int x = 0; x <= 50; x++) {
+            if (x == 25 && y == 10) // Точка пересечения
+                std::cout << "+";
+            else if (y == 10) // Горизонтальная ось
+                std::cout << "-";
+            else if (x == 25) // Вертикальная ось
+                std::cout << "|";
+            else if (x == 50 && y == 10) // Стрелка вправо
+                std::cout << ">";
+            else if (x == 25 && y == 0) // Стрелка вверх
+                std::cout << "^";
+            else
+                std::cout << " ";
+        }
+        std::cout << "\n";
+    }
+    return 0;
+}
