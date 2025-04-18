bb7f418cbba3a4c56159fc4b206734c754638f17
Yuri Konev <crazy_clown93@mail.ru>
2025-04-18 22:07:25 +0300

    8

---
+++ HW_7/Task_8/Task_8.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    int height;
+
+    
+    std::cout << "Введите высоту треугольника (положительное целое число): ";
+    std::cin >> height;
+
+    
+    while (height <= 0) {
+        std::cout << "Высота должна быть положительным целым числом. Попробуйте снова: ";
+        std::cin >> height;
+    }
+
+    
+    for (int i = 1; i <= height; i++) {
+        
+        for (int j = 1; j <= height - i; j++) {
+            std::cout << " ";
+        }
+        
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            std::cout << "#";
+        }
+        
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
