1d4464cc0d330729f8a060cf917da682de160d31
Yuri Konev <crazy_clown93@mail.ru>
2025-04-25 22:59:53 +0300

    Task 6

---
+++ HW_8/Task_6/Task_6.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    double start, end;
+    int count = 0;
+
+    std::cout << "Введите начальную амплитуду: ";
+    std::cin >> start;
+    std::cout << "Введите конечную амплитуду: ";
+    std::cin >> end;
+
+    while (start > end) {
+        start *= 0.916; // уменьшение амплитуды на 8,4%
+        ++count;
+    }
+
+    std::cout << "Маятник качнется " << count << " раз(а).\n";
+
+    return 0;
+}
