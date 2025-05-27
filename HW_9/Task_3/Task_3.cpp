798673d3196608d3c5cc9fdad84e5ec235943102
Yuri Konev <crazy_clown93@mail.ru>
2025-05-27 19:21:15 +0300

    Task 3

---
+++ HW_9/Task_3/Task_3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+
+int main() {
+    std::string input;
+
+    std::cout << "Enter a string: ";
+    std::getline(std::cin, input); // Считываем всю строку, включая пробелы
+
+
+    int wordCount = 0; // Переменная для подсчета количества слов
+    std::istringstream iss(input); // Используем istringstream для разбора ввода
+
+    // Используем цикл for для извлечения слов
+    for (std::string word; iss >> word; ++wordCount) {
+        // Цикл будет продолжаться для каждого найденного слова
+    }
+
+    std::cout << "Answer: " << wordCount << std::endl; 
+
+    return 0;
+}
