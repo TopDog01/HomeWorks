8c511e9594886cc32c973f07f2fa9b3d8e4aa9f5
Yuri Konev <crazy_clown93@mail.ru>
2025-05-31 19:41:42 +0300

    Task

---
+++ HW_10/Task/Task.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+    float a, b, c;
+    std::cout << "a, b, c:";
+    std::cin >> a >> b >> c; // читаем значение c
+
+    if (a == 0) // Проверяем на ноль
+    {
+        std::cout << "Not a quadratic equation!" << std::endl;
+    }
+    else
+    {
+        float discriminant = b * b - 4 * a * c; // исправлена формула дискриминанта
+
+        if (discriminant > 0) // сюда должна вести проверка на больше 0
+        {
+            float x1 = (-b + std::sqrt(discriminant)) / (2 * a); // исправлено
+            float x2 = (-b - std::sqrt(discriminant)) / (2 * a); // исправлено
+            std::cout << "Root 1, 2: " << x1 << ", " << x2 << std::endl; // исправлено
+        }
+        else if (discriminant == 0)
+        {
+            float x = -b / (2 * a); // исправлено
+            std::cout << "Root: " << x << std::endl; // исправлено на x
+        }
+        else
+        {
+            std::cout << "Complex scenario is not supported!" << std::endl;
+        }
+    }
+}
