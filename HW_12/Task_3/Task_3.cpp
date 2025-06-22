1a57c9435808ff900a4cbb80d195a522a0cb0686
Yuri Konev <crazy_clown93@mail.ru>
2025-06-22 21:12:11 +0300

    Task3

---
+++ HW_12/Task_3/Task_3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cassert>
+
+float travelTime(float distance, float speed) {
+    assert(speed > 0); 
+    return distance / speed; 
+}
+
+int main() {
+    float distance, speed;
+
+    std::cout << "Enter distance (in kilometers): ";
+    std::cin >> distance;
+
+    std::cout << "Enter speed (in km/h): ";
+    std::cin >> speed;
+
+    
+    try {
+        float time = travelTime(distance, speed);
+        std::cout << "Travel time: " << time << " hours" << std::endl;
+    }
+    catch (...) {
+        std::cerr << "Error: Speed must be positive." << std::endl;
+    }
+
+    return 0;
+}
